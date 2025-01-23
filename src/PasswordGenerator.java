8436ef51f87fb75de65f8cac4baa144b4d2e499b
MarcoGreco74 <marcogreco.gibson@gmail.com>
2025-01-23 17:16:31 +0100

    fix: implementazione di Scanner

--- src/PasswordGenerator.java
+++ src/PasswordGenerator.java
@@ -11,17 +11,31 @@ Il programma deve fare quanto segue:
 Esempio: ho un utente che si chiama Pinco Pallo, nato il 12/05/1994, il cui colore preferito è il magenta
 La sua password sarà Pinco-Pallo-magenta-2011
 */
+import java.util.Scanner;
+
 public class PasswordGenerator {
     public static void main(String[] args) throws Exception {
-		String nome = "Marco";
-		String cognome = "Greco";
-		String colore = "blu";
-		int giorno = 7;
-		int mese = 10;
-		int anno = 1974;		
-		char dash = '-';
-		int birthDate = giorno+mese+anno;
-		String password = nome+dash+cognome+dash+colore+dash+birthDate;	
-		System.out.println("La tua password è: "+password);
-    }
-}
+		try {
+			Scanner scan = new Scanner(System.in);
+			System.out.println("Inserisci il tuo nome");
+			String nome = scan.next();
+			System.out.println("Inserisci il tuo cognome");
+			String cognome = scan.next();
+			System.out.println("Inserisci il tuo colore preferito");
+			String colore = scan.next();
+			System.out.println("Inserisci il tuo giorno di nascita");
+			int giorno = scan.nextInt();
+			System.out.println("Inserisci il tuo mese di nascita");
+			int mese = scan.nextInt();
+			System.out.println("Inserisci il tuo anno di nascita");
+			int anno = scan.nextInt();
+			char dash = '-';
+			int birthDate = giorno+mese+anno;
+			String password = nome+dash+cognome+dash+colore+dash+birthDate;
+			System.out.println("La tua password è: "+password);
+      		scan.close();
+    		}catch (ExceptionInInitializerError e) {
+      			System.out.println("An error occurred.");  
+    		} 
+  		}
+	}
